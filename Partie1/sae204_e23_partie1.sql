3681ddafcc09140bdac38c44fc6812b776125898
Arthur Chauvel <arthurchauvel207@gmail.com>
2025-04-25 10:20:29 +0200

    avancee creation tables

--- Partie1/sae204_e23_partie1.sql
+++ Partie1/sae204_e23_partie1.sql
@@ -1,3 +1,28 @@
-CREATE SCHEMA "PARTIE1-SAE204";
+CREATE SCHEMA "partie1";
 
-SET SCHEMA 'PARTIE1-SAE204';
+SET SCHEMA 'partie1';
+
+DROP TABLE individu CASCADE;
+CREATE TABLE individu (
+	id_individu INT PRIMARY KEY,
+	nom VARCHAR,
+	prenom VARCHAR,
+	date_naissance DATE,
+	code_postal VARCHAR,
+	ville VARCHAR,
+	sexe CHAR,
+	nationalite VARCHAR,
+	INE VARCHAR
+);
+
+CREATE TABLE etudiant (
+	code_nip VARCHAR PRIMARY KEY,
+	cat_socio_etu VARCHAR,
+	cat_socio_parent VARCHAR,
+	bourse_superieur BOOLEAN,
+	mention_bac VARCHAR,
+	serie_bac VARCHAR,
+	dominante_bac VARCHAR,
+	specialite_bac VARCHAR,
+	mois_annee_obtention_bac VARCHAR(7)
+);
