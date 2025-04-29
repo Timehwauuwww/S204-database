0713874d15b8c29d0ca752f58017846aef1c9d04
Arthur Chauvel <arthurchauvel207@gmail.com>
2025-04-29 08:40:13 +0200

    Squashed commit of the following:
    commit c08ebf3b28c277fd6d47492ee2d7eb6e78d1aaa2
    Author: Arthur Chauvel <arthurchauvel207@gmail.com>
    Date:   Tue Apr 29 08:35:43 2025 +0200
    
        création tables semestre et module
    
    commit 4c248b215ee99fd2c55ee6e905a2f1f265ed4cc6
    Author: Arthur Chauvel <arthurchauvel207@gmail.com>
    Date:   Tue Apr 29 08:28:24 2025 +0200
    
        fin table etudiant

--- Partie1/sae204_e23_partie1.sql
+++ Partie1/sae204_e23_partie1.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA "partie1";
 
 SET SCHEMA 'partie1';
 
-CREATE TABLE individu (
+CREATE TABLE _individu (
 	id_individu INT PRIMARY KEY,
 	nom VARCHAR,
 	prenom VARCHAR,
@@ -23,22 +23,19 @@ CREATE TABLE _etudiant (
 	serie_bac VARCHAR,
 	dominante_bac VARCHAR,
 	specialite_bac VARCHAR,
-	mois_annee_obtention_bac VARCHAR(7)
+	mois_annee_obtention_bac VARCHAR(7),
+	id_individu INT,
+	FOREIGN KEY (id_individu) REFERENCES individu(id_individu) 
 );
 
-CREATE TABLE _candidat(
-	no_candidat INT PRIMARY KEY,
-	classement VARCHAR,
-	boursier_lycee VARCHAR,
-	profil_candidat VARCHAR,
-	etablissement VARCHAR,
-	dept_etablissement VARCHAR,
-	ville_etablissement VARCHAR,
-	niveau_etude VARCHAR,
-	type_formation VARCHAR,
-	serie_prec VARCHAR,
-	dominante_prec VARCHAR,
-	specialite_prec VARCHAR,
-	lv1 VARCHAR,
-	lv2 VARCHAR
+CREATE TABLE _semestre (
+	id_semestre INT PRIMARY KEY,
+	num_semestre VARCHAR(5),
+	annee_univ VARCHAR(9)
+);
+
+CREATE TABLE _module (
+	id_module VARCHAR(5) PRIMARY KEY,
+	libelle_module VARCHAR,
+	ue VARCHAR(2)
 );
